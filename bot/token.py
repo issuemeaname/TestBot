ca3291131ed9038e80ca9ecc8267ab6492ac1bc3
issuemeaname <dj3skulls@hotmail.com>
2019-04-19 10:27:03 +0100

    Setup for Heroku deployment

---
+++ bot/token.py
@@ -0,0 +1,4 @@
+# Usually this is not uploaded but for testing purposes it is
+#
+# Note that this token is temporary and will be disabled and removed
+TOKEN = "NTY4NzEyMzc4Njk1NTQ4OTU0.XLmFkg.2bb-1xrG18AZ9g9sUE21iZapgEo"
